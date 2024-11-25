faf634109fd31edf3de3a8d0590bc87fea4f8769
Andrej Dortmann <andrej.dortmann25@gmail.com>
2024-11-25 11:55:52 +0100

    feat: add fetch

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { IQuiz } from "./interfaces/IQuiz";
 
-const difficultyRadios = document.getElementsByName("difficulty") as NodeListOf<HTMLInputElement>;
-const languageRadios = document.getElementsByName("language") as NodeListOf<HTMLInputElement>;
-const BASE_URL = "https://vz-wd-24-01.github.io/typescript-quiz/questions/";
 
+
+const languageRadios = document.getElementsByName("language") as NodeListOf<HTMLInputElement>;
+const difficultyRadios = document.getElementsByName("difficulty") as NodeListOf<HTMLInputElement>;
+const BASE_URL = "https://vz-wd-24-01.github.io/typescript-quiz/questions/";
+const labelEasy = document.getElementById("label-easy") as HTMLInputElement;
+const labelHard = document.getElementById("label-hard") as HTMLInputElement;
 let question:IQuiz;
 
-const getSelectedValue = (radios: NodeListOf<HTMLInputElement>): string => {
+const getSelectedValue = (radios: NodeListOf<HTMLInputElement>) => {
   for (const radio of radios) {
       if (radio.checked) {
           return radio.value;
@@ -15,17 +18,33 @@ const getSelectedValue = (radios: NodeListOf<HTMLInputElement>): string => {
   return "";
 };
 
-const handleChange = () => {
-  const selectedDifficulty = getSelectedValue(difficultyRadios);
-  const selectedLanguage = getSelectedValue(languageRadios);
+const renderRadioBox = (language: string) => {
+  if (!labelEasy || !labelHard) return;
 
+  if (language === "de") {
+    labelEasy.textContent = "Leicht";
+    labelHard.textContent = "Schwer";
+    difficultyRadios[0].value = "leicht";
+    difficultyRadios[1].value = "schwer";
+  } else {
+    labelEasy.textContent = "Easy";
+    labelHard.textContent = "Hard";
+    difficultyRadios[0].value = "easy";
+    difficultyRadios[1].value = "hard";
+  }
+};
+
+const handleChange = () => {
+  const selectedLanguage = getSelectedValue(languageRadios);
+  const selectedLanguageValue = selectedLanguage;
+  const selectedDifficulty = getSelectedValue(difficultyRadios);
+  renderRadioBox(selectedLanguageValue);
+  fetchAllData(selectedDifficulty)
 };
 
 
 difficultyRadios.forEach((radio) => radio.addEventListener("change", handleChange));
 languageRadios.forEach((radio) => radio.addEventListener("change", handleChange));
-
-
 
 const fetchAllData = async (url:string) => {
   const newUrl = `${BASE_URL + url + ".json"}`;
